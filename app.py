0e2134f0423e7bf8d7e45fcc1e9ad0246b58175f
574n13y <vivesht@gmail.com>
2025-03-20 15:31:19 +0000

    app code completed

--- app.py
+++ app.py
@@ -127,4 +127,15 @@ items = [
     }
 ]
 
+@app.route('/')
+def home():
+    return render_template('index.html', items=items)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
